2d9508eec1f5feac3809cd8ad471b04446b4cfb1
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-09-19 19:57:03 -0400

    Adding function

--- script.js
+++ script.js
@@ -89,49 +89,67 @@ const CourseInfo = {
     // if an assignment is not yet due, it should not be included in either
     // the average or the keyed dictionary of scores
 
-    // learner_id: 125,                        learner_id: 132,
-    // assignment_id: 1,                       assignment_id: 1,
-    // submission: {                           submission: {
-    //   submitted_at: "2023-01-25",              submitted_at: "2023-01-24",
-    //   score: 47                                score: 39
 
-    // learner_id: 125,                        learner_id: 132,
-    // assignment_id: 2,                       assignment_id: 2,
-    // submission: {                           submission: {
-    //   submitted_at: "2023-02-12",               submitted_at: "2023-03-07",
-    //   score: 150                                core: 140
-      
-    // learner_id: 125,
-    // assignment_id: 3,
-    // submission: {
-    //   submitted_at: "2023-01-25",
-    //   score: 400  
-      
-// // empty  object for the learner data
-// let learnerData = {
-//     id: ''
-// }
 
-  try {
-    if(CourseInfo.id !== AssignmentGroup.course_id){
-        console.log(`Input invalid`)
-    }
-} catch (err) {
-    console.error(err)
-}
+
+// AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error,
 
 // for(let assignment = AssignmentGroup; assignment > 0;){
 //     if(AssignmentGroup.assignments.points_possible !== number || AssignmentGroup.assignments.points_possible <= 0)
 // } else if(points_possible <= 0)
 
-for(let i = 0; i < AssignmentGroup.assignments; i++){
-    let assignment = AssignmentGroup.assignments[i]
-    if(typeof assignment.points_possible !== 'number' || assignment.points_possible <= 0){
-        console.log('Invalid: The assignment does not have a valid value')
-    } else {
-        break;
+console.log(getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions))
+
+function getLearnerData (course, ag, submissions){
+    // console.log(CourseInfo, AssignmentGroup, LearnerSubmissions)
+    let result = []
+    try {
+        if(course.id !== ag.course_id){
+            throw `Input invalid`
+        }
+
+        let temp = 0;
+
+        for(let sub of submissions){
+            // console.log(sub)
+            if(!result.some(el => el.id == sub.learner_id)){ //if learner doesn't exist
+                let student = {id: sub.learner_id}
+                student[sub.assignment_id] = sub.submission.score
+                result.push(student)
+            } else { // if a learner does exist
+                
+                result.forEach((s) => {
+                    if(s.id == sub.learner_id){
+                        s[sub.assignment_id] = sub.submission.score
+                        temp += sub.submission.score
+                        // console.log(s[sub.assignment_id])
+                    }
+                })
+                
+            }
+        }
+        console.log(temp / 3)
+
+
+
+
+
+    } catch (err) {
+        console.error(err)
     }
+
+
+    return result
 }
+// for(let j = 0; j < AssignmentGroup.assignments.length; j++){
+//     let assignment = AssignmentGroup.assignments[j]
+//     if(typeof assignment.points_possible !== 'number' || assignment.points_possible <= 0){
+//         console.error('Invalid: The assignment does not have a valid value')
+//     } else {
+//         typeof assignment.points_possible == 'number' || assignment.points_possible >= 0
+//         continue;
+//     }
+// }
 
 //  What do we know 
     // need to creata function called getLearnerData
@@ -139,27 +157,102 @@ for(let i = 0; i < AssignmentGroup.assignments; i++){
     // thro error if 
 
 
-// let learnerData = {
-//     learner_id125: {
-//         totalScore: 0,
-//         totalPoint: 0,
-//         a1: pScore,
-//         a2: pscore,
-//     },
-//     learner_id132: {
-//         totalScore: 0,
-//         totalPoint: 0,
-//         a1: pScore,
-//         a2: pscore,
-//     }
-// }
 
-// getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions)
 
-// function getLearnerData (CourseInfo, AssignmentGroup, LearnerSubmissions){
-//     // console.log(CourseInfo, AssignmentGroup, LearnerSubmissions)
-//     let result = []
+//objects = 4 data types : CourseInfo, AssignmentGroup, LearnerSubmission, AssignmentInfo
+//compare due dates and get the average
+
+//compare (manipulate array)
+//iterate over objects to pull into an array
+//308.3 loops example
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i);
 // }
+let arr = []
+
+for (let i = 0; i <  AssignmentGroup.assignments.length; i++) {
+	//access the object
+    const ag = AssignmentGroup.assignments[i];
+    const subDate = {}; //trying to get due_at
+    subDate.due_at = ag.due_at;
+
+    //pushing array 
+    arr.push(subDate)
+}
+console.log(arr)
+
+// same loop for retrieving point
+// attempt to put in a funcion later
+let ptsp = []
+
+for (let p = 0; p <  AssignmentGroup.assignments.length; p++) {
+	//access the object
+    const pt = AssignmentGroup.assignments[p];
+    const points = {}; //trying to get point_possible
+    points.points_possible = pt.points_possible;
+
+    //pushing array 
+    ptsp.push(points)
+}
+console.log(ptsp)
+
+// trying to get the total score
+// let scoreTotal = []
+
+// for (let sub2 in LearnerSubmissions) {
+//     let sub3 = sub2[0];
+// 	//access the object
+//     const sT = sub2[0].score;
+//     const LearnerS = {}; //trying to get score
+//     LearnerS.sub3 = sT;
+
+//     //pushing array 
+//     scoreTotal.push(LearnerS)
+// }
+// console.log(scoreTotal)
+
+
+// trying to get the average grade
+// let due = AssignmentGroup.assignments
+// let sub = LearnerSubmissions.submission
+let allScore = 0;
+let allCount = 0;
+
+for(let due of AssignmentGroup.assignments){
+    for(let sub of LearnerSubmissions){
+        let sub1 = sub.submission.submitted_at
+        
+        if(due.due_at < sub1){
+            let lateGrade = sub.submission.score * 0.10
+            allScore += lateGrade;
+            allCount ++;
+
+            // lateGrade = sub.submission.score -= lateGrade
+            // return lategrade
+
+        } else if(due.due_at >= sub1){
+            let grade = sub.submission.score + sub.submission.score
+            return grade
+
+        }else if(grade > 0 && lateGrade > 0){
+            let total = grade + grade || lateGrade + grade
+            let average = total / 2
+            return average
+        }
+    }
+console.log(average)
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -191,43 +284,3 @@ for(let i = 0; i < AssignmentGroup.assignments; i++){
 //   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
 //   console.log(result);
-  
-
-//objects = 4 data types : CourseInfo, AssignmentGroup, LearnerSubmission, AssignmentInfo
-//compare due dates and get the average
-
-//compare (manipulate array)
-//iterate over objects to pull into an array
-//308.3 loops example
-// for (let i = 0; i < 10; i++) {
-// 	console.log(i);
-// }
-
-let arr = []
-
-for (let i = 0; i <  AssignmentGroup.assignments.length; i++) {
-	//access the object
-    const ag = AssignmentGroup.assignments[i];
-    const subDate = {}; //trying to get due_at
-    subDate.due_at = ag.due_at;
-
-    //pushing array 
-    arr.push(subDate)
-}
-console.log(arr)
-
-// same loop for retrieving point
-// attempt to put in a funcion later
-let ptsp = []
-
-for (let p = 0; p <  AssignmentGroup.assignments.length; p++) {
-	//access the object
-    const pt = AssignmentGroup.assignments[p];
-    const points = {}; //trying to get due_at
-    points.points_possible = pt.points_possible;
-
-    //pushing array 
-    ptsp.push(points)
-}
-console.log(ptsp)
-
